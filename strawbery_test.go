9ede243d0e48ba6b21a20281e098dff1bdd99f28
井上大誠 <t.base.8901@icloud.com>
2021-03-07 15:55:19 +0900

    [RED]三角推量 文字列表現の取得

--- strawbery_test.go
+++ strawbery_test.go
@@ -24,3 +24,13 @@ func Test_品種あまおうサイズLのいちごから文字列表現を取得
 
 	assert.Equal(t, "あまおう: L", actual)
 }
+
+func Test_品種もういっこサイズMのいちごから文字列表現を取得できる(t *testing.T) {
+	sut := Strawbery{
+		kind: "もういっこ",
+		size: "M",
+	}
+	actual := sut.String()
+
+	assert.Equal(t, "もういっこ: M", actual)
+}

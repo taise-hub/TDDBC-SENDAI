0d7ce260db2149e31c314d5ff2c7596a3266d7d5
井上大誠 <t.base.8901@icloud.com>
2021-03-07 16:59:37 +0900

    リファクタリング

--- strawbery_test.go
+++ strawbery_test.go
@@ -7,38 +7,73 @@ import (
 )
 
 func Test_StrawveryString(t *testing.T) {
-	t.Run("品種:あまおう、サイズ:Lのいちごから文字列表現「あまおう: L」を取得できる", func(t *testing.T) {
-		sut := Strawbery{
-			kind: "あまおう",
-			size: "L",
-		}
-		actual := sut.String()
+	type args struct {
+		kind string
+		size string
+	}
+	tests := map[string]struct {
+		args     args
+		expected string
+	}{
+		"品種:あまおう、サイズ:Lのいちごから文字列表現「あまおう: L」を取得できる": {
+			args:     args{kind: "あまおう", size: "L"},
+			expected: "あまおう: L",
+		},
+		"品種:あまおう、サイズ:LLのいちごから文字列表現「あまおう: LL」を取得できる": {
+			args:     args{kind: "あまおう", size: "LL"},
+			expected: "あまおう: LL",
+		},
+		"品種:とちおとめ、サイズ:Lのいちごから文字列表現「とちおとめ: L」を取得できる": {
+			args:     args{kind: "とちおとめ", size: "L"},
+			expected: "とちおとめ: L",
+		},
+		"品種:もういっこ、サイズMのいちごから文字列表現「もういっこ: M」を取得できる": {
+			args:     args{kind: "もういっこ", size: "M"},
+			expected: "もういっこ: M",
+		},
+		"品種:もういっこ、サイズMのいちごから文字列表現「もういっこ: S」を取得できる": {
+			args:     args{kind: "もういっこ", size: "S"},
+			expected: "もういっこ: S",
+		},
+	}
 
-		assert.Equal(t, "あまおう: L", actual)
-	})
+	for tName, test := range tests {
+		t.Run(tName, func(t *testing.T) {
+			sut := Strawbery{
+				kind: test.args.kind,
+				size: test.args.size,
+			}
+			actual := sut.String()
 
-	t.Run("品種:もういっこ、サイズMのいちごから文字列表現「もういっこ: M」を取得できる", func(t *testing.T) {
-		sut := Strawbery{
-			kind: "もういっこ",
-			size: "M",
-		}
-		actual := sut.String()
+			assert.Equal(t, test.expected, actual)
+		})
+	}
 
-		assert.Equal(t, "もういっこ: M", actual)
-	})
 }
 
 func Test_StrawveryWeight(t *testing.T) {
 	t.Run("重さが0gの時エラー", func(t *testing.T) {
 		_, err := New("あまおう", 0)
-
 		assert.NotEmpty(t, err)
 	})
 
-	t.Run("重さが1gの時サイズはS", func(t *testing.T) {
-		sut, _ := New("あまおう", 1)
-		actual := sut.Size()
-
-		assert.Equal(t, "S", actual)
-	})
+	type args struct {
+		kind   string
+		weight uint
+	}
+	tests := map[string]struct {
+		args     args
+		expected string
+	}{
+		"重さが1gの時サイズはS": {
+			args:     args{kind: "あまおう", weight: 1},
+			expected: "S",
+		},
+	}
+	for tName, test := range tests {
+		t.Run(tName, func(t *testing.T) {
+			actual, _ := New(test.args.kind, test.args.weight)
+			assert.Equal(t, test.expected, actual.Size())
+		})
+	}
 }
